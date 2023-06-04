adb81ef8cc7e77f12df4574c55c091a7b08ba466
HAMIDI Mohamed Ayman <108700861+aymanhamidi@users.noreply.github.com>
2023-06-04 21:08:44 +0200

    Add files via upload

---
+++ OcpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
